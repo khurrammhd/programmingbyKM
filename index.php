6b56defd80d2b4be67c938ac81a37a1f0be5e962
Khurram Mahmood <khurrammhd@gmail.com>
2023-03-16 20:45:06 +0500

    Change to show configurations

--- index.php
+++ index.php
@@ -5,3 +5,4 @@ echo "My second commit";
 echo "commit to show conflicts";
 echo "this change is on Feature branch only";
 echo "This is another change on the Feature branch";
+echo "Change to check configuration";

5228adb86f6aa67bdbe7e34aa9435b2aa271346a
Khuram M <khurrammhd@gmail.com>
2023-03-16 20:07:51 +0500

    This is the second change on feature branch

--- index.php
+++ index.php
@@ -4,3 +4,4 @@ echo "My first commit";
 echo "My second commit";
 echo "commit to show conflicts";
 echo "this change is on Feature branch only";
+echo "This is another change on the Feature branch";
